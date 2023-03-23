aa5c379ff47a9c5f911e825f706b9660757d84b8
sdragos007 <sandu5433@gmail.com>
2023-03-23 12:51:22 -0400

    mesaj

---
+++ app/View/Components/Navbar.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Navbar extends Component
+{
+    public array $navbarLinks;
+    
+    public function __construct(array $navbarLinks)
+    {
+        $this->navbarLinks = $navbarLinks;
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('components.navbar');
+    }
+}
